f9ad0ab26c03bb68f2cf8beb3905ed3a1d86cbce
Bruno Mendes Campanati <bruno.campanate@al.infnet.edu.br>
2021-06-11 18:27:31 -0300

    Modify Book Review model

--- BookReviewing.Entities/Models/BookReview.cs
+++ BookReviewing.Entities/Models/BookReview.cs
@@ -1,11 +1,15 @@
-﻿namespace BookReviewing.Entities.Models
+﻿using System;
+
+namespace BookReviewing.Entities.Models
 {
     public class BookReview
     {
         public int Id { get; set; }
-        public double Score { get; set; }
+        public float Score { get; set; }
         public string Comment { get; set; }
         public int BookId { get; set; }
         public int UserId { get; set; }
+        public DateTime DatePosted { get; set; }
+        public DateTime LastUpdate { get; set; }
     }
 }
