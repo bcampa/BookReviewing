df0f726967483483ef569c1aac1e74b3796f60e1
Bruno Mendes Campanati <bruno.campanate@al.infnet.edu.br>
2021-06-11 18:07:11 -0300

    Add Entities class library and Book Review model

---
+++ BookReviewing.Entities/Models/BookReview.cs
@@ -0,0 +1,11 @@
+﻿namespace BookReviewing.Entities.Models
+{
+    public class BookReview
+    {
+        public int Id { get; set; }
+        public double Score { get; set; }
+        public string Comment { get; set; }
+        public int BookId { get; set; }
+        public int UserId { get; set; }
+    }
+}
