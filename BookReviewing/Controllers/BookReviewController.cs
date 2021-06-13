f1ed24930349b08756f5520430143f4c0e119b1f
Bruno Mendes Campanati <bruno.campanate@al.infnet.edu.br>
2021-06-12 22:05:26 -0300

    Add controllers and rename project

---
+++ BookReviewing/Controllers/BookReviewController.cs
@@ -0,0 +1,37 @@
+﻿using BookReviewing.Entities.Models;
+using BookReviewing.Entities.Repositories;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BookReviewing.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BookReviewController : ControllerBase
+    {
+        private readonly BookReviewRepository _repository;
+
+        public BookReviewController()
+        {
+            _repository = new BookReviewRepository();
+        }
+
+        public IActionResult Get()
+        {
+            var bookReviews = _repository.GetAll();
+            return Ok(bookReviews);
+        }
+
+        [HttpPost]
+        public IActionResult Add([FromBody] BookReview bookReview)
+        {
+            _repository.Add(bookReview);
+            _repository.SaveChanges();
+            return Ok();
+        }
+    }
+}
--- BookReviewing/Program.cs
+++ BookReviewing/Program.cs
@@ -7,7 +7,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
-namespace BookReviewing
+namespace BookReviewing.Api
 {
     public class Program
     {
