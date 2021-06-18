dd66172ac57241206853d6bfcc7988ffcb1aa105
Bruno Mendes Campanati <bruno.campanate@al.infnet.edu.br>
2021-06-17 22:53:24 -0300

    Add more consumers

---
+++ BookReviewing.Api/Consumers/BookRemovedConsumer.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace BookReviewing.Api.Consumers
+{
+    public class BookRemovedConsumer : RabbitMqListener<BookRemovedMessage>
+    {
+        public BookRemovedConsumer() : base("book-removed") { }
+
+        protected override void HandleMessage(BookRemovedMessage message)
+        {
+            // Do nothing
+        }
+    }
+
+    public class BookRemovedMessage
+    {
+        public int BookId { get; set; }
+    }
+}
---
+++ BookReviewing.Api/Consumers/UserCreatedConsumer.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace BookReviewing.Api.Consumers
+{
+    public class UserCreatedConsumer : RabbitMqListener<UserCreatedMessage>
+    {
+        public UserCreatedConsumer() : base("user-created") { }
+
+        protected override void HandleMessage(UserCreatedMessage message)
+        {
+            // Do nothing
+        }
+    }
+
+    public class UserCreatedMessage
+    {
+        public int UserId { get; set; }
+        public DateTime CreatedAt { get; set; }
+    }
+}
