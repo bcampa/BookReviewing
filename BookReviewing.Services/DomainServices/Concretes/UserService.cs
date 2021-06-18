e78d3ddd377d0aafc711e3554b91698678ce0f36
Bruno Mendes Campanati <bruno.campanate@al.infnet.edu.br>
2021-06-18 00:09:44 -0300

    Add Book Service and User Service

---
+++ BookReviewing.Services/DomainServices/Concretes/UserService.cs
@@ -0,0 +1,28 @@
+﻿using BookReviewing.Entities.Models;
+using BookReviewing.Entities.Repositories.Contracts;
+using BookReviewing.Services.DomainServices.Contracts;
+using BookReviewing.Services.Messages.User;
+
+namespace BookReviewing.Services.DomainServices.Concretes
+{
+    public class UserService : IUserService
+    {
+        private readonly IUserRepository _userRepository;
+
+        public UserService(IUserRepository userRepository)
+        {
+            _userRepository = userRepository;
+        }
+
+        public void AddUser(UserCreatedMessage message)
+        {
+            var entity = new User
+            {
+                Id = message.UserId
+            };
+
+            _userRepository.Add(entity);
+            _userRepository.SaveChanges();
+        }
+    }
+}
---
+++ BookReviewing.Services/DomainServices/Contracts/IUserService.cs
@@ -0,0 +1,9 @@
+﻿using BookReviewing.Services.Messages.User;
+
+namespace BookReviewing.Services.DomainServices.Contracts
+{
+    public interface IUserService
+    {
+        void AddUser(UserCreatedMessage message);
+    }
+}
