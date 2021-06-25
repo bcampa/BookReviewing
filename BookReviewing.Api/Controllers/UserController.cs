8fa21a999f723e8cbea95b4a31ca4a73bae8d134
Bruno Mendes Campanati <bruno.campanate@al.infnet.edu.br>
2021-06-24 23:24:28 -0300

    Document book controller and user controller

--- BookReviewing.Api/Controllers/UserController.cs
+++ BookReviewing.Api/Controllers/UserController.cs
@@ -1,7 +1,9 @@
 ﻿using BookReviewing.Entities.Models;
 using BookReviewing.Entities.Repositories.Contracts;
+using BookReviewing.Services.Dto.Misc;
 using BookReviewing.Services.Dto.User;
 using BookReviewing.Shared.Filters;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using System;
 
@@ -18,16 +20,31 @@ namespace BookReviewing.Api.Controllers
             _repository = repository;
         }
 
+        /// <summary>
+        /// Gets multiple users according to the submitted filter. This endpoint should only be used for debugging.
+        /// </summary>
+        /// <param name="filter"></param>
+        /// <returns>The users matching the filter</returns>
+        /// <response code="200">Returns the users matching the filter</response>
         [HttpGet]
+        [ProducesResponseType(typeof(PaginatedResponse<User>), StatusCodes.Status200OK)]
         public IActionResult Get([FromQuery] PaginationFilter filter)
         {
             filter ??= new PaginationFilter();
 
             var users = _repository.GetMany(filter);
-            return Ok(users);
+            var response = new PaginatedResponse<User>(filter, users);
+            return Ok(response);
         }
 
+        /// <summary>
+        /// Creates a new User. This endpoint should only be used for debugging.
+        /// </summary>
+        /// <param name="request"></param>
+        /// <returns>A newly created user</returns>
+        /// <response code="200">Returns the newly created user</response>
         [HttpPost]
+        [ProducesResponseType(typeof(User), StatusCodes.Status200OK)]
         public IActionResult Add([FromBody] CreateUserRequest request)
         {
             var user = new User
